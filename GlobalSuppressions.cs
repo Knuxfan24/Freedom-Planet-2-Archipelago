c42d7ae0ddeb25ad221ad079345266d27b65508f
Knux <knuxfan@live.co.uk>
2025-11-20 07:14:16 +0000

    Update GlobalSuppressions.cs
    Accidentally undid setting the justification and scope for the naming styles suppression.

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -6,4 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0066:Convert switch statement to expression", Justification = "Not a fan of Switch Expressions personally.", Scope = "module")]
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~P:Freedom_Planet_2_Archipelago.CustomData.MenuConnection.CacheJSON.item_name_to_id")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "I'll name things how I want to Microsoft thank you very much (plus you're bitching about something that's named that way to match up with a JSON file so...).", Scope = "module")]
