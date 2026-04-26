46b937905a07acd98bc83442dc3f4f25297d4361
Knux <knuxfan@live.co.uk>
2026-04-26 06:23:20 +0100

    Changed how Spam Trap Messages are stored
    Having two separate string arrays is dumb when a simple custom class can do the job in a much neater way.
    
    Haven't tested because laziness.

--- CustomData/SpamTrap.cs
+++ CustomData/SpamTrap.cs
@@ -4,66 +4,49 @@ namespace Freedom_Planet_2_Archipelago.CustomData
 {
     internal class SpamTrap : FPBaseObject
     {
-        // The various spam messages that can be picked for display.
-        private static readonly string?[] headers =
+        private class SpamTrapMessage(string? header, string message)
+        {
+            /// <summary>
+            /// Text shown in the header. If this is null, then the header is removed entirely and the message body shifted up.
+            /// </summary>
+            public string? Header = header;
+
+            /// <summary>
+            /// Text shown in the message body.
+            /// </summary>
+            public string Message { get; set; } = message;
+        }
+
+        // The various messages that can be picked for display.
+        private static readonly SpamTrapMessage[] messages =
         [
-            "NOTICE",
-            "Beauty Contest",
-            "CONGRATULATIONS",
-            "Advertisment",
-            null,
-            null,
-            "Adventure Awaits",
-            "Buzz Cola",
-            "Advertisment",
-            null,
-            null,
-            "A MYURRDERRRR?!",
-            null,
-            "CONGRATULATIONS",
-            "Thief Alert!",
-            null,
-            "Did You Know?",
-            null,
-            "Message from Ghandi",
-            "Did You Know?",
-            "Shrine of Chance",
-            "The Ocean",
-            null,
-            "Need Reception?",
-            "AURORA BOREALIS",
-            "Dear [PLAYER NAME]",
-            "ALERT"
-        ];
-        private static readonly string[] messages =
-        [
-            "We've been trying\r\nto reach you\r\nregarding your\r\nbike's extended\r\nwarranty.",
-            "You have won second\r\nprize in a\r\nbeauty contest\r\n\r\nCollect $10",
-            "YOU'RE THE 50,000TH\r\nVISITOR TO ZAO LAND!\r\n\r\nCLICK HERE TO CLAIM\r\nYOUR PRIZE!",
-            "Half price entry to\r\nForest Frontiers.\r\n\r\nAvaliable while\r\nstocks last.", // Reference to OpenRCT2.
-            "You won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?", // Reference to FNaF World.
-            "AAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA",
-            "Explore the Keep\r\ntoday.\r\n\r\nThis message\r\nsponsored by\r\nThe Keymaster.", // Reference to Keymaster's Keep.
-            "For humans!\r\n\r\nIsn't posionous to\r\nanybody!\r\n(that we know of...)", // Reference to The Simpsons: Hit and Run.
-            "75% off your next\r\npurchase at JojaMart.", // Reference to Stardew Valley.
-            "Receiving this\r\nSpam Trap...\r\n\r\nIt fills you with\r\ndetermination.", // Reference to Undertale
-            "You feel an evil\r\npresence watching\r\nyou...", // Reference to Terraria.
-            "ON MY OWL EXPRESS?!", // Reference to A Hat in Time.
-            "eastmost peninsula\r\nis the secret", // Reference to The Legend of Zelda.
-            "You've won your\r\nvery own mansion.\r\n\r\nClick here for\r\ndetails!", // Reference to Luigi's Mansion.
-            "The word\r\n[PLAYER NAME],\r\nthey stole it too!", // Reference to Kingdom Hearts 2 apparently, think the original word is Photo?
-            "You want fun?\r\n[PLAYER NAME]\r\nwill show you fun...",
-            "There's a Mew\r\nunder the truck.", // Reference to Pokémon Red.
-            "i showed you my\r\ncacodemon plz\r\nrespond", // Reference to Doom.
-            "Our words are backed\r\nby nuclear weapons!", // Reference to Civilization.
-            "Metal Harbor is\r\nactually beatable\r\nwithout the\r\nLight Shoes!", // Reference to Sonic Adventure 2: Battle.
-            "You offer to the\r\nshrine, but gain\r\nnothing.", // Reference to Risk of Rain 2.
-            "Now with 75%\r\nmore Leviathan!", // Reference to Subnautica
-            "This advert\r\ndedicated to those\r\nwho perished on\r\nthe climb...", // Reference to Celeste
-            "Climb to the top\r\nof Hawk Peak!", // Reference to A Short Hike.
-            "At this time of year?\r\nAt this time of day?\r\nIn this part of the\r\nmutliworld?\r\n\r\nLocalised entirely\r\nwithin your slot data?!", // You know what this is a reference to...
-            "Please come to the\r\ncastle. I've baked\r\na cake for you.\r\nYours truly--\r\nPrincess Toadstool", // Reference to Super Mario 64.
-            "[PLAYER NAME]\r\nhas died in an\r\naccident on\r\nSteeplechase 1!", // Also a reference to OpenRCT2.
+            new("NOTICE", "We've been trying\r\nto reach you\r\nregarding your\r\nbike's extended\r\nwarranty."),
+            new("Beauty Contest", "You have won second\r\nprize in a\r\nbeauty contest\r\n\r\nCollect $10"),
+            new("CONGRATULATIONS", "YOU'RE THE 50,000TH\r\nVISITOR TO ZAO LAND!\r\n\r\nCLICK HERE TO CLAIM\r\nYOUR PRIZE!"),
+            new("Advertisment", "Half price entry to\r\nForest Frontiers.\r\n\r\nAvaliable while\r\nstocks last."), // Reference to OpenRCT2.
+            new(null, "You won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?\r\nYou won't get tired\r\nof my voice will you?"), // Reference to FNaF World.
+            new(null, "AAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA\r\nAAAAAAAAAAAAAAAAAAAA"),
+            new("Adventure Awaits", "Explore the Keep\r\ntoday.\r\n\r\nThis message\r\nsponsored by\r\nThe Keymaster."), // Reference to Keymaster's Keep.
+            new("Buzz Cola", "For humans!\r\n\r\nIsn't posionous to\r\nanybody!\r\n(that we know of...)"), // Reference to The Simpsons: Hit and Run.
+            new("Advertisment", "75% off your next\r\npurchase at JojaMart."), // Reference to Stardew Valley.
+            new(null, "Receiving this\r\nSpam Trap...\r\n\r\nIt fills you with\r\ndetermination."), // Reference to Undertale
+            new(null, "You feel an evil\r\npresence watching\r\nyou..."), // Reference to Terraria.
+            new("A MYURRDERRRR?!", "ON MY OWL EXPRESS?!"), // Reference to A Hat in Time.
+            new(null, "eastmost peninsula\r\nis the secret"), // Reference to The Legend of Zelda.
+            new("CONGRATULATIONS", "You've won your\r\nvery own mansion.\r\n\r\nClick here for\r\ndetails!"), // Reference to Luigi's Mansion.
+            new("Thief Alert!", "The word\r\n[PLAYER NAME],\r\nthey stole it too!"), // Reference to Kingdom Hearts 2.
+            new(null, "You want fun?\r\n[PLAYER NAME]\r\nwill show you fun..."), 
+            new("Did You Know?", "There's a Mew\r\nunder the truck."), // Reference to Pokémon Red.
+            new(null, "i showed you my\r\ncacodemon plz\r\nrespond"), // Reference to Doom.
+            new("Message from Ghandi", "Our words are backed\r\nby nuclear weapons!"), // Reference to Civilization.
+            new("Did You Know?", "Metal Harbor is\r\nactually beatable\r\nwithout the\r\nLight Shoes!"), // Reference to Sonic Adventure 2: Battle.
+            new("Shrine of Chance", "You offer to the\r\nshrine, but gain\r\nnothing."), // Reference to Risk of Rain 2.
+            new("The Ocean", "Now with 75%\r\nmore Leviathan!"), // Reference to Subnautica
+            new(null, "This advert\r\ndedicated to those\r\nwho perished on\r\nthe climb..."), // Reference to Celeste
+            new("Need Reception?", "Climb to the top\r\nof Hawk Peak!"), // Reference to A Short Hike.
+            new("AURORA BOREALIS", "At this time of year?\r\nAt this time of day?\r\nIn this part of the\r\nmutliworld?\r\n\r\nLocalised entirely\r\nwithin your slot data?!"), // Reference to that Simpsons meme.
+            new("Dear [PLAYER NAME]", "Please come to the\r\ncastle. I've baked\r\na cake for you.\r\nYours truly--\r\nPrincess Toadstool"), // Reference to Super Mario 64.
+            new("ALERT", "[PLAYER NAME]\r\nhas died in an\r\naccident on\r\nSteeplechase 1!") // Also a reference to OpenRCT2.
         ];
 
         // The valid colours to tint the background.
@@ -102,15 +85,15 @@ namespace Freedom_Planet_2_Archipelago.CustomData
             genericTimer = Plugin.rng.Next(3, 6);
 
             // Select the message to display.
-            var messageIndex = Plugin.rng.Next(messages.Length);
+            int messageIndex = Plugin.rng.Next(messages.Length);
 
             // Select a colour for the background.
             gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().color = colours[Plugin.rng.Next(colours.Length)];
 
             // If the target message has a header, then just update its text.
             // If not, then hide the header element and shift the body element up by 8 pixels.
-            if (headers[messageIndex] != null)
-                gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = headers[messageIndex];
+            if (messages[messageIndex].Header != null)
+                gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = messages[messageIndex].Header;
             else
             {
                 gameObject.transform.GetChild(1).gameObject.SetActive(false);
@@ -118,7 +101,7 @@ namespace Freedom_Planet_2_Archipelago.CustomData
             }
 
             // Update the body element's text.
-            gameObject.transform.GetChild(2).GetComponent<TextMesh>().text = messages[messageIndex];
+            gameObject.transform.GetChild(2).GetComponent<TextMesh>().text = messages[messageIndex].Message;
 
             // Swap out [PLAYER NAME] in the header with our name.
             gameObject.transform.GetChild(1).GetComponent<TextMesh>().text = gameObject.transform.GetChild(1).GetComponent<TextMesh>().text.Replace("[PLAYER NAME]", Plugin.session.Players.GetPlayerName(Plugin.session.ConnectionInfo.Slot));
